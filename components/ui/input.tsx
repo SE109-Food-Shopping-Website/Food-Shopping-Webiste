4ae604a4eb0805855befc35a450d056c64ea6ee8
DangThiNgocMinh <ngocminh87049@gmail.com>
2025-03-03 18:01:14 +0700

    set up project

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,20 +2,23 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <div className="w-[500px] h-[50px] p-5 rounded-[5px] border border-[#5CB338] justify-start items-center inline-flex overflow-hidden">
+        <input
+          type={type}
+          className={cn(
+            "w-full grow shrink basis-0 text-black placeholder:text-[#CCCCCC] text-base font-normal bg-transparent outline-none",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    )
+  }
+)
+Input.displayName = "Input"
 
 export { Input }
