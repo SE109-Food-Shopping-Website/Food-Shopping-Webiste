df59a269bd79d78d3ec9b403bd4da9de2d2032d9
Phluynhh <22520778@gm.uit.edu.vn>
2025-03-09 08:49:36 +0700

    update provider

--- app/admin/manage/provider/update/[id]/page.tsx
+++ app/admin/manage/provider/update/[id]/page.tsx
@@ -1,5 +1,165 @@
-import React from "react";
+"use client";
 
-export default function addProvider() {
-  return <div>page</div>;
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { LogOut, Save } from "lucide-react";
+import { useForm } from "react-hook-form";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string(),
+  email: z.string(),
+  phone: z.string(),
+  address: z.string(),
+  status: z.string(),
+});
+
+export default function updateProvider() {
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
+
+  function onSubmit(values: z.infer<typeof schema>) {
+    console.log(values);
+  }
+  return (
+    <div>
+      <div className="relative justify-start text-black text-base font-normal font-['Inter']">
+        Trung tâm / Quản lý / Nhà cung cấp
+      </div>
+      <div className="relative justify-start text-[#5cb338] text-base font-bold font-['Inter'] mt-[10px]">
+        Thông tin nhà cung cấp
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          encType="multipart/form-data"
+        >
+          <div className="w-full self-stretch inline-flex justify-between items-center mt-[10px]">
+            <div className="w-[500px] inline-flex flex-col justify-start items-start gap-5">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel className="font-normal">
+                      Tên nhà cung cấp
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="Công ty ABC" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="w-[500px] inline-flex flex-col justify-start items-start gap-5">
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel className="font-normal">
+                      Địa chỉ nhà cung cấp
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="123 Thủ Đức" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className="w-full self-stretch inline-flex justify-between items-center mt-[10px]">
+            <div className="w-[500px] flex flex-col justify-start items-start gap-5">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel className="font-normal">
+                      Email nhà cung cấp
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        className="h-[60px]"
+                        placeholder="abc@gmail.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="w-[500px] h-[40px] flex flex-col justify-start items-start gap-5 mb-[35px]">
+              <FormField
+                control={form.control}
+                name="status"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel className="font-normal">Trạng thái</FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger className="w-full h-[40px] p-5">
+                          <SelectValue placeholder="Active" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="active">Active</SelectItem>
+                          <SelectItem value="inactive">Inactive</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <div className="w-full self-stretch self-stretch inline-flex flex-col justify-start items-end gap-5 overflow-hidden mt-[15px]">
+            <div className="inline-flex justify-start items-start gap-[29px]">
+              <div className="relative">
+                <LogOut className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white" />
+                <Button variant={"secondary"} className="pl-12" type="button">
+                  <Link href="/admin/manage/provider" className="text-white">
+                    Thoát
+                  </Link>
+                </Button>{" "}
+              </div>
+              <div className="relative">
+                <Save className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white" />
+                <Button className="pl-12" type="submit">
+                  <Link href="/admin/manage/provider/add">Lưu</Link>
+                </Button>{" "}
+              </div>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 }
