401e4a87babfa17c546316da321daaf4e353f858
phnglinhh <linhtran@cybertech.com.vn>
2025-04-03 23:55:38 +0700

    create mock data for product

---
+++ app/admin/manage/product/data/product-data.ts
@@ -0,0 +1,28 @@
+export const productData = [
+    {
+        id: 1,
+        name: "Trứng gà 1 quả",
+        type: "Trứng",
+        provider: "Bách Hóa Xanh",
+        unit: "Quả",
+        description: "Trứng gà tươi",
+        img_links: [
+            "/product/1/egg.jpg",
+            "/product/1/egg_2.png",
+            "/product/1/egg_3.jpeg"
+        ],
+    },
+    {
+        id: 2,
+        name: "Sữa Milo 1 hộp",
+        type: "Sữa",
+        provider: "Bách Hóa Xanh",
+        unit: "Hộp",
+        description: "Sữa Milo ngon",
+        img_links: [
+            "/product/2/milo.jpg",
+            "/product/2/milo_2.jpg",
+            "/product/2/milo_3.jpg"
+        ],
+    }
+]
