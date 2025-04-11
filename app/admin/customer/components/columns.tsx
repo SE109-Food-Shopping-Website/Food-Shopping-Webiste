8cd92340d64a5a89d2343f56ece2a9fb5d49ab9b
phnglinhh <linhtran@cybertech.com.vn>
2025-04-11 07:57:20 +0700

    customer list screen

---
+++ app/admin/customer/components/columns.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+import { Eye, Pencil, Trash } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+export interface Customer {
+  id: string;
+  email: string;
+  name: string;
+  phone: string;
+  address: string;
+  birthday: string;
+  gender: string;
+  isActive: boolean;
+}
+
+export const columns: ColumnDef<Customer>[] = [
+  {
+    accessorKey: "id",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          ID
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("id")}</div>,
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          Email
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          Tên khách hàng
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("name")}</div>,
+  },
+  {
+    accessorKey: "phone",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          Số điện thoại
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("phone")}</div>,
+  },
+  {
+    accessorKey: "address",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          Địa chỉ
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("address")}</div>,
+  },
+  {
+    accessorKey: "birthday",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          Ngày sinh
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("birthday")}</div>,
+  },
+  {
+    accessorKey: "gender",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          Giới tính
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("gender")}</div>,
+  },
+  {
+    accessorKey: "isActive",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+        >
+          Hoạt động
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const isActive = row.getValue("isActive");
+
+      return (
+        <Badge status={isActive ? "active" : "inactive"}>
+          {isActive ? "Hoạt động" : "Không hoạt động"}
+        </Badge>
+      );
+    },
+  },
+];
--- app/admin/customer/page.tsx
+++ app/admin/customer/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
 import React from "react";
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
+import { customerData } from "./data/customer-data";
 
 export default function pageCustomer() {
-  return <div>page</div>;
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState<string | undefined>(undefined);
+
+  return (
+    <div className="relative justify-start text-black text-base font-normal font-['Inter']">
+      Góp ý / Khách hàng / Danh sách
+      <DataTable
+        columns={columns}
+        data={customerData}
+        isLoading={isLoading}
+        error={error}
+      />
+    </div>
+  );
 }
