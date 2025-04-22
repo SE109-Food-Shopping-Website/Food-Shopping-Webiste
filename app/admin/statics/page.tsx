6b5a2ffc1f337d64371b9cc9e9f77f0cfc7238d9
phnglinhh <linhtran@cybertech.com.vn>
2025-04-22 12:09:11 +0700

    some changes

--- app/admin/statics/page.tsx
+++ app/admin/statics/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function pageStatics() {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="justify-start text-black text-base font-normal font-['Inter']">
+        Trung tâm / Thống kê / Doanh thu
+      </div>
+    </div>
+  );
 }
